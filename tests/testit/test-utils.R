3b41cb99bcd2c629cb19c5a9eae3a04697da04ab
Yihui Xie <xie@yihui.name>
2016-11-30 00:00:38 -0600

    add some tests

---
+++ tests/testit/test-utils.R
@@ -0,0 +1,18 @@
+library(testit)
+
+assert(
+  'sample2() does not make an exception on length-1 x',
+  sample2(5, 2) %==% c(5, 5),
+  length(sample2(1:5, 2)) == 2
+)
+
+assert(
+  'protect_math() puts math in ` `',
+  protect_math('$a$') %==% '`\\(a\\)`',
+  protect_math('$500 $600') %==% '$500 $600',
+  protect_math('$ a $') %==% '$ a $',
+  protect_math('`$a$`') %==% '`$a$`',
+  protect_math('$$a$$') %==% '`$$a$$`',
+  protect_math('$$ a$$') %==% '$$ a$$',
+  protect_math('$$a$') %==% '$$a$'
+)

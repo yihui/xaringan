a4db6912c4393aefaa7198d01e5734f8e21c2a2e
Yihui Xie <xie@yihui.name>
2018-02-19 14:09:29 -0600

    add a fake example just to pass the invisible CRAN check

--- R/render.R
+++ R/render.R
@@ -64,6 +64,8 @@
 #' @references \url{http://naruto.wikia.com/wiki/Tsukuyomi}
 #' @importFrom htmltools tagList tags htmlEscape HTML
 #' @export
+#' @examples
+#' # rmarkdown::render('foo.Rmd', 'xaringan::moon_reader')
 moon_reader = function(
   css = c('default', 'default-fonts'), self_contained = FALSE, seal = TRUE, yolo = FALSE,
   chakra = 'https://remarkjs.com/downloads/remark-latest.min.js', nature = list(),

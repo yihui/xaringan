f5219e973479940ee517f334c0028d31d97f52c1
pat-s <patrick.schratz@gmail.com>
2018-11-01 11:01:30 +0100

    delete test

--- tests/testit/export_pdf.R
+++
@@ -1,7 +0,0 @@
-library(testit)
-
-assert(
-  "check if the PDF conversion works",
-  export_pdf("inst/rmarkdown/templates/xaringan/skeleton/skeleton.html", "~/out.pdf",
-             decktape_version = "2.8.0")
-)

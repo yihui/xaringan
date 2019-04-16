dda23ac4853c5c7928254ae3278cc31727134793
Susan Vanderplas <srvanderplas@gmail.com>
2019-04-16 12:21:59 -0500

    self-contained images for xaringan, with tests

--- tests/testit/test-utils.R
+++ tests/testit/test-utils.R
@@ -36,3 +36,18 @@ assert(
   # A space is added in following (can't overwrite first space when 2nd char is *)
   highlight_code(" * paste('a') #<<") %==% "* * paste('a')"
 )
+
+test_uri = "data:text/plain;base64,R0lGODdhAgACAIAAAAAAAP///ywAAAAAAgACAAACAoRRADs="
+assert(
+  "process_self_contained_images handles image specifications correctly",
+  process_self_contained_images("background-image: url(https://www.htmlgoodies.com/images/1x1.gif)") %==%
+    paste0("background-image: url(", test_uri, ")"),
+  process_self_contained_images("testing, testing <img src='https://www.htmlgoodies.com/images/1x1.gif'/>") %==%
+    paste0("testing, testing <img src='", test_uri, "'/>"),
+  process_self_contained_images("testing, testing `<img src='https://www.htmlgoodies.com/images/1x1.gif'/>`") %==%
+    "testing, testing `<img src='https://www.htmlgoodies.com/images/1x1.gif'/>`",
+  process_self_contained_images("testing, testing !()[https://www.htmlgoodies.com/images/1x1.gif]") %==%
+    paste0("testing, testing !()[https://www.htmlgoodies.com/images/1x1.gif]"),
+  process_self_contained_images("testing, testing `!()[https://www.htmlgoodies.com/images/1x1.gif]`") %==%
+    "testing, testing `!()[https://www.htmlgoodies.com/images/1x1.gif]`"
+)

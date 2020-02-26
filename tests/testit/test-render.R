5ccc30166864b7c28d2a690b7e3e768f74ee9f03
Garrick Aden-Buie <garrick.aden-buie@moffitt.org>
2020-02-26 10:49:04 -0500

    Add tests for parameterized infinite_moon_reader()
    Checks that params are used and over-written correctly when using paramaterized slides. Default values can be over-written at run time via `params` argument in infinite_moon_reader().
    
    These tests also use the ... passing to rmarkdown::render() to set additional parameters, such as choosing the output file and rendering the slides Rmd quietly.

---
+++ tests/testit/test-render.R
@@ -0,0 +1,41 @@
+library(testit)
+
+tmpin = tempfile(fileext = ".Rmd")
+file.copy("parameterized.Rmd", tmpin)
+tmpout = tempfile(fileext = ".html")
+tmpout_file = basename(tmpout)
+
+options(servr.daemon = TRUE)
+s = xaringan::inf_mr(tmpin,
+                     cast_from = dirname(tmpout),
+                     output_file = tmpout_file,
+                     quiet = TRUE)
+s$stop_server()
+params_top_level = readLines(tmpout)
+
+count_matches = function(pattern, text) sum(grepl(pattern, text, fixed = TRUE))
+
+assert('parameterized slides uses top-level `params` in YAML', {
+  (count_matches("params$foo = oof", params_top_level) %==% 1L)
+  (count_matches("params$bar = rab", params_top_level) %==% 1L)
+  (count_matches("params$baz = zab", params_top_level) %==% 1L)
+})
+
+s = xaringan::inf_mr(tmpin,
+                     cast_from = dirname(tmpout),
+                     params = list(foo = "hello", bar = "world"),
+                     output_file = tmpout_file,
+                     quiet = TRUE)
+s$stop_server()
+params_top_level = readLines(tmpout)
+
+assert('inf_mr(...) overrides `params` in YAML', {
+  (count_matches("params$foo = oof", params_top_level) %==% 0L)
+  (count_matches("params$foo = hello", params_top_level) %==% 1L)
+  (count_matches("params$bar = rab", params_top_level) %==% 0L)
+  (count_matches("params$bar = world", params_top_level) %==% 1L)
+})
+
+assert('inf_mr(...) params falls back to top-level `params` in YAML', {
+  (count_matches("params$baz = zab", params_top_level) %==% 1L)
+})

2eecd0ab1f6380958e82389159e6aaafd26aebe6
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-03-04 14:01:18 +0000

    Comment out scripts in slides that aren't executed
    and update the function documentation

--- inst/rmarkdown/templates/xaringan/resources/js/script-tags.js
+++ inst/rmarkdown/templates/xaringan/resources/js/script-tags.js
@@ -1,11 +1,11 @@
 (function() {
   "use strict"
-  /* Move <script> tags from slides to end of document
+  /* Replace <script> tags in slides area to make them executable
    *
-   * Runs after post-processing of markdown source into slides and moves only
+   * Runs after post-processing of markdown source into slides and replaces only
    * <script>s on the last slide of continued slides using the .has-continuation
-   * class added by xaringan. Finally, all <script>s in the slides area are
-   * removed with a note that they can be found at the end of <body>.
+   * class added by xaringan. Finally, any <script>s in the slides area that
+   * aren't executed are commented out.
    */
   var scripts = document.querySelectorAll(
     '.remark-slides-area .remark-slide-container:not(.has-continuation) script'
@@ -16,5 +16,13 @@
     var code = document.createTextNode(scripts[i].textContent);
     s.appendChild(code);
     scripts[i].parentElement.replaceChild(s, scripts[i]);
+  }
+  var scriptsNotExecuted = document.querySelectorAll(
+    '.remark-slides-area .remark-slide-container.has-continuation script'
+  );
+  if (!scriptsNotExecuted.length) return;
+  for (var i = 0; i < scriptsNotExecuted.length; i++) {
+    var comment = document.createComment(scriptsNotExecuted[i].outerHTML)
+    scriptsNotExecuted[i].parentElement.replaceChild(comment, scriptsNotExecuted[i])
   }
 })();

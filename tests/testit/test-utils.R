5b52b8814b47b5772586bfbc145ffd01b9fb4b30
Susan Vanderplas <srvanderplas@gmail.com>
2020-03-05 16:11:01 -0600

    updated documentation, remove tests that were redundant.

--- tests/testit/test-utils.R
+++ tests/testit/test-utils.R
@@ -23,8 +23,7 @@ assert('highlight_code handles {{ .code. }} and ...code #<< formats', {
   (highlight_code(' * paste(1) #<<') %==% '* * paste(1)')
 })
 
-test_uri <-
-  "data:text/plain;base64,R0lGODdhAgACAIAAAAAAAP///ywAAAAAAgACAAACAoRRADs="
+test_uri <- "data:image/gif;base64,R0lGODdhAgACAIAAAAAAAP///ywAAAAAAgACAAACAoRRADs="
 assert(
   "process_self_contained_images handles image specifications correctly",
   process_self_contained_images(
@@ -47,23 +46,4 @@ assert(
   process_self_contained_images(
     "testing `!()[https://www.htmlgoodies.com/images/1x1.gif]`") %==%
     "testing `!()[https://www.htmlgoodies.com/images/1x1.gif]`"
-)
-
-test_uri = "data:image/gif;base64,R0lGODdhAgACAIAAAAAAAP///ywAAAAAAgACAAACAoRRADs="
-assert(
-  "process_self_contained_images handles image specifications correctly",
-  process_self_contained_images("background-image: url(https://www.htmlgoodies.com/images/1x1.gif)") %==%
-    paste0("background-image: url(", test_uri, ")"),
-  process_self_contained_images("background-image: ../1x1.gif") %==%
-    paste0("background-image: ", test_uri),
-  process_self_contained_images("testing, testing <img src='https://www.htmlgoodies.com/images/1x1.gif'/>") %==%
-    paste0("testing, testing <img src='", test_uri, "'/>"),
-  process_self_contained_images("testing, testing <img src='https://www.htmlgoodies.com/images/1x1.gif'></img>") %==%
-    paste0("testing, testing <img src='", test_uri, "'></img>"),
-  process_self_contained_images("testing, testing `<img src='https://www.htmlgoodies.com/images/1x1.gif'/>`") %==%
-    "testing, testing `<img src='https://www.htmlgoodies.com/images/1x1.gif'/>`",
-  process_self_contained_images("testing, testing !()[https://www.htmlgoodies.com/images/1x1.gif]") %==%
-    paste0("testing, testing !()[https://www.htmlgoodies.com/images/1x1.gif]"),
-  process_self_contained_images("testing, testing `!()[https://www.htmlgoodies.com/images/1x1.gif]`") %==%
-    "testing, testing `!()[https://www.htmlgoodies.com/images/1x1.gif]`"
 )

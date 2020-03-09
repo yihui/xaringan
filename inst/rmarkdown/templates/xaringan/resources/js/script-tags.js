b6b47c646bac94e65d5dbb50f3c56b238235f65d
Yihui Xie <xie@yihui.name>
2020-03-09 09:35:18 -0500

    add missing commas

--- inst/rmarkdown/templates/xaringan/resources/js/script-tags.js
+++ inst/rmarkdown/templates/xaringan/resources/js/script-tags.js
@@ -9,9 +9,9 @@
     var s = document.createElement('script');
     var code = document.createTextNode(scripts[i].textContent);
     s.appendChild(code);
-    var scriptAttrs = scripts[i].attributes
+    var scriptAttrs = scripts[i].attributes;
     for (var j = 0; j < scriptAttrs.length; j++) {
-      s.setAttribute(scriptAttrs[j].name, scriptAttrs[j].value)
+      s.setAttribute(scriptAttrs[j].name, scriptAttrs[j].value);
     }
     scripts[i].parentElement.replaceChild(s, scripts[i]);
   }

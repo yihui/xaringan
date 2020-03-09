5b475a8500a73ab5799de10204bf050a38562a54
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-03-09 13:30:42 +0000

    Copy attributes of scripts when replacing in slides
    Fixes #257

--- inst/rmarkdown/templates/xaringan/resources/js/script-tags.js
+++ inst/rmarkdown/templates/xaringan/resources/js/script-tags.js
@@ -15,6 +15,10 @@
     var s = document.createElement('script');
     var code = document.createTextNode(scripts[i].textContent);
     s.appendChild(code);
+    var scriptAttrs = scripts[i].attributes
+    for (var j = 0; j < scriptAttrs.length; j++) {
+      s.setAttribute(scriptAttrs[j].name, scriptAttrs[j].value)
+    }
     scripts[i].parentElement.replaceChild(s, scripts[i]);
   }
   var scriptsNotExecuted = document.querySelectorAll(

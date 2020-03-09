f7ccd70fccd3b5845fbc52f241b6fa21f70c2a4a
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-03-09 10:40:12 -0400

    Fix #257: ensure attributes are copied to replaced script tags (#258)
    * Copy attributes of scripts when replacing in slides
    
    Fixes #257
    
    * Replace all scripts in slides regardless of continuation
    
    * add missing commas
    
    * Update NEWS.md
    
    Co-authored-by: Yihui Xie <xie@yihui.name>

--- inst/rmarkdown/templates/xaringan/resources/js/script-tags.js
+++ inst/rmarkdown/templates/xaringan/resources/js/script-tags.js
@@ -1,28 +1,18 @@
 (function() {
   "use strict"
-  /* Replace <script> tags in slides area to make them executable
-   *
-   * Runs after post-processing of markdown source into slides and replaces only
-   * <script>s on the last slide of continued slides using the .has-continuation
-   * class added by xaringan. Finally, any <script>s in the slides area that
-   * aren't executed are commented out.
-   */
+  // Replace <script> tags in slides area to make them executable
   var scripts = document.querySelectorAll(
-    '.remark-slides-area .remark-slide-container:not(.has-continuation) script'
+    '.remark-slides-area .remark-slide-container script'
   );
   if (!scripts.length) return;
   for (var i = 0; i < scripts.length; i++) {
     var s = document.createElement('script');
     var code = document.createTextNode(scripts[i].textContent);
     s.appendChild(code);
+    var scriptAttrs = scripts[i].attributes;
+    for (var j = 0; j < scriptAttrs.length; j++) {
+      s.setAttribute(scriptAttrs[j].name, scriptAttrs[j].value);
+    }
     scripts[i].parentElement.replaceChild(s, scripts[i]);
-  }
-  var scriptsNotExecuted = document.querySelectorAll(
-    '.remark-slides-area .remark-slide-container.has-continuation script'
-  );
-  if (!scriptsNotExecuted.length) return;
-  for (var i = 0; i < scriptsNotExecuted.length; i++) {
-    var comment = document.createComment(scriptsNotExecuted[i].outerHTML)
-    scriptsNotExecuted[i].parentElement.replaceChild(comment, scriptsNotExecuted[i])
   }
 })();

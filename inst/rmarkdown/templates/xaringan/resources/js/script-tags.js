c8c62948b4e5f23dcc1ff22b39aa001447aa3d49
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-03-09 13:32:04 +0000

    Replace all scripts in slides regardless of continuation

--- inst/rmarkdown/templates/xaringan/resources/js/script-tags.js
+++ inst/rmarkdown/templates/xaringan/resources/js/script-tags.js
@@ -1,14 +1,8 @@
 (function() {
   "use strict"
-  /* Replace <script> tags in slides area to make them executable
-   *
-   * Runs after post-processing of markdown source into slides and replaces only
-   * <script>s on the last slide of continued slides using the .has-continuation
-   * class added by xaringan. Finally, any <script>s in the slides area that
-   * aren't executed are commented out.
-   */
+  // Replace <script> tags in slides area to make them executable
   var scripts = document.querySelectorAll(
-    '.remark-slides-area .remark-slide-container:not(.has-continuation) script'
+    '.remark-slides-area .remark-slide-container script'
   );
   if (!scripts.length) return;
   for (var i = 0; i < scripts.length; i++) {
@@ -20,13 +14,5 @@
       s.setAttribute(scriptAttrs[j].name, scriptAttrs[j].value)
     }
     scripts[i].parentElement.replaceChild(s, scripts[i]);
-  }
-  var scriptsNotExecuted = document.querySelectorAll(
-    '.remark-slides-area .remark-slide-container.has-continuation script'
-  );
-  if (!scriptsNotExecuted.length) return;
-  for (var i = 0; i < scriptsNotExecuted.length; i++) {
-    var comment = document.createComment(scriptsNotExecuted[i].outerHTML)
-    scriptsNotExecuted[i].parentElement.replaceChild(comment, scriptsNotExecuted[i])
   }
 })();
